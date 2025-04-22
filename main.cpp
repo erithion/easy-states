b8f676f9ad834d44e8e296238e9db26d3f533f8c
Artem Bondartsov <artem.bondartsov@tietoevry.com>
2025-04-22 23:50:54 +0300

    easy-states with example of use

---
+++ main.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file main.cpp
+ * @author Artem Bondartsov (erithion@gmail.com)
+ * @brief Example of using easy_states state machine
+ * @version 0.1
+ * @date 2025-03-22
+ * 
+ * @copyright Copyright (c) 2025
+ */
+#include "easy_states.hpp"
+#include <numeric>
+#include <type_traits>
+#include <fmt/format.h>
+#include <magic_enum.hpp>
+
+enum struct States
+{
+    Locked = 0,
+    Unlocked
+};
+
+enum struct Events
+{
+    Push = 0,
+    Coin
+};
+
+template <>
+constexpr States std::numeric_limits<States>::max() { return States::Unlocked; }
+
+template <>
+constexpr Events std::numeric_limits<Events>::max() { return Events::Coin; }
+
+template <typename Enum>
+struct fmt::formatter<Enum, std::enable_if_t<std::is_enum_v<Enum>, char>> : fmt::formatter<std::string_view>
+{
+    auto format(Enum e, fmt::format_context& ctx) const
+    {
+        return fmt::format_to(ctx.out(), "{}", magic_enum::enum_name(e));
+    }
+};
+
+template <States S>
+using State = Const<States, S>;
+
+template <Events E>
+using Event = Const<Events, E>;
+
+State<States::Locked> enter(State<States::Locked>  src, Event<Events::Push> evt)
+{
+    fmt::println("{} --[{}]--> {}", src.value, evt.value, "Locked");
+    return {};
+}
+
+State<States::Unlocked>    enter(State<States::Locked> src, Event<Events::Coin>     evt)
+{
+    fmt::println("{} --[{}]--> {}", src.value, evt.value, "Unlocked");
+    return {};
+}
+
+State<States::Locked>  enter(State<States::Unlocked> src, Event<Events::Push>   evt)
+{
+    fmt::println("{} --[{}]--> {}", src.value, evt.value, "Locked");
+    return {};
+}
+
+int main()
+{
+    auto state = States::Locked;
+
+    state = next(state, Events::Push);
+    state = next(state, Events::Coin);
+    state = next(state, Events::Push);
+    state = next(state, Events::Push);
+
+    state = next(state, Events::Coin);
+    state = next(state, Events::Coin);
+    state = next(state, Events::Push);
+    state = next(state, Events::Push);
+    return 0;
+}
